fd19e055b04206eee43cdc5ab5c08fd3dd2e7e2f
DANIEL886886 <danielparaschiv886@gmail.com>
2024-07-03 14:36:09 +0300

    project update

--- task-viewer/src/App.js
+++ task-viewer/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import React from "react";
 import { data } from "./components/app-data/data";
 import TaskList from "./components/task-list/task-list";
 
 function App() {
 
-	return (
-		<TaskList tasklist={data} />
-	);
+
+	return React.createElement(TaskList,{tasklist:data})
+
+	// return (
+	// 	<TaskList tasklist={data} />
+	// );
 
 }
 
--- task-viewer/src/components/task-card/TaskCard.js
+++ task-viewer/src/components/task-card/TaskCard.js
@@ -3,6 +3,14 @@ import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+
+  let btnclick = props.id
+
+  const handleClick=() => {
+    console.log("Click from " + props.id);
+    btnclick = props.id + "Clicked !"
+  }
+
   return (
     <div className="card-wrapper">
 
@@ -16,6 +24,7 @@ function TaskCard(props) {
       </div>
 
       <div className="card-footer">
+        <button onClick={handleClick}>Click me!</button>
         <DateContainer date={props.dueDate} />
       </div>
       
